2b4f0b4ba0e8e360fcd0740a032ccbb1638c0484
PradeepRaj369 <pradeepraj1906@gmail.com>
2024-07-03 12:34:03 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Header from "./Components/Header";
+import SignUp from "./Components/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import About from "./Components/About";
+import Login from "./Components/Login";
+import Account from "./Components/Account";
+import ProductDetails from "./Components/ProductDetails";
+import CheckOut from "./Components/CheckOut";
+import Cart from "./Components/Cart";
+import WishList from "./Components/WishList";
+import Error404 from "./Components/Error404";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={ <About />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/product-details" element={<ProductDetails />} />
+          <Route path="/checkout" element={<CheckOut />}/>
+          <Route path="/cart" element={<Cart />}/>
+          <Route path="/wishlist" element={<WishList />}/>
+          <Route path="*" element={<Error404 />}/>
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  Container,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../CSS/header.css";
+const Header = () => {
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center bg-dark text-light gap-5 header-section1">
+        <p className="mt-2 fs-6">
+          Summer Sale For All Swim Suits And Free Express Delivery-OFF 50%!{" "}
+          <span className="fw-900 ml-3">
+            <a
+              href=""
+              style={{
+                textDecoration: "none",
+                color: "white",
+                fontWeight: "bold",
+              }}
+            >
+              ShopNow
+            </a>
+          </span>
+        </p>
+        <select name="" id="" className="bg-dark text-light border-0">
+          <option value="">English</option>
+        </select>
+      </div>
+      <section className="header-section">
+        <Container className="p-2 pt-4">
+          <Navbar className="justify-content-around">
+            <div>
+              <h3 className="fw-600">Exclusive</h3>
+            </div>
+            <Nav className="gap-3 nav-links">
+              <Link to="/home" className="nav-link">
+                Home
+              </Link>
+              <Link to="/contact" className="nav-link">
+                Contact
+              </Link>
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+              <Link to="/signup" className="nav-link">
+                Sign Up
+              </Link>
+            </Nav>
+            <div className="d-flex  align-items-center gap-4">
+              <div className="search-bar">
+                <input
+                  type="text"
+                  placeholder="what are you looking for?"
+                  id="nav-search"
+                />
+                <span className="search-icon">
+                  <i class="bi bi-search"></i>
+                </span>
+              </div>
+              <Nav.Link className="">
+                <i class="bi bi-heart fs-5"></i>
+              </Nav.Link>
+              <Nav.Link className="">
+                <i class="bi bi-cart3 fs-5"></i>
+              </Nav.Link>
+            </div>
+          </Navbar>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Header;
