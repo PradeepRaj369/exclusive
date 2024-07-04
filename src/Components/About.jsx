82021361a2e524df37fbc31acc7da9bd8126a2d7
jayapriyathimmappa <jayapriyavt@gmail.com>
2024-07-04 11:45:23 +0530

    about Page1

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import aboutimg from "../Components/image/aboutimg.png";
+import aboutsec from "../Components/image/aboutsec.png";
+import aboutsec1 from "../Components/image/aboutsec1.png";
+import aboutsec2 from "../Components/image/about2.png";
 import "../CSS/About.css";
 
 const About = () => {
@@ -34,35 +37,121 @@ const About = () => {
         </div>
         <div className="aboutfirst">
           <div className="aboutfirstchild">
-            <img src="" alt="" />
+            <div className="aboutthirdimg">
+              <div className="aboutthirdimg1">
+                <i class="bi bi-shop-window"></i>
+              </div>
+            </div>{" "}
+            <br />
             <h1>10.5k</h1>
             <p>Sallers active our site</p>
           </div>
-          <div className="aboutfirstchild">
-            <img src="" alt="" />
+          <div className="aboutfirstchild aboutfirstdiff">
+            <div className="aboutdiff">
+              <div className="aboutdiff1">
+                <i class="bi bi-currency-dollar"></i>
+              </div>
+            </div>{" "}
+            <br />
             <h1>33k</h1>
             <p>Mopnthly Produduct Sale</p>
           </div>
           <div className="aboutfirstchild">
-            <img src="" alt="" />
+            <div className="aboutthirdimg">
+              <div className="aboutthirdimg1">
+                <i class="bi bi-bag-heart-fill"></i>
+              </div>
+            </div>{" "}
+            <br />
             <h1>45.5k</h1>
             <p>Customer active in our site</p>
           </div>
           <div className="aboutfirstchild">
-            <img src="" alt="" />
+            <div className="aboutthirdimg">
+              <div className="aboutthirdimg1">
+                <i class="bi bi-coin"></i>
+              </div>
+            </div>{" "}
+            <br />
             <h1>25k</h1>
             <p>Anual gross sale in our site</p>
           </div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="aboutsecond">
+          <div className="aboutsecondchild">
+            <img id="aboutsec" src={aboutsec} alt="aboutsecond" />
+            <h1>Tom Cruise</h1>
+            <p>Founder & Chairman</p>
+            <span>
+              <i class="bi bi-instagram"></i>
+            </span>
+            <span>
+              <i class="bi bi-whatsapp"></i>
+            </span>
+            <span>
+              <i class="bi bi-linkedin"></i>
+            </span>
+          </div>
+          <div className="aboutsecondchild">
+            <img id="aboutsec" src={aboutsec1} alt="aboutsecond" />
+            <h1>Emma Watson</h1>
+            <p>Managing Director</p>
+            <span>
+              <i class="bi bi-instagram"></i>
+            </span>
+            <span>
+              <i class="bi bi-whatsapp"></i>
+            </span>
+            <span>
+              <i class="bi bi-linkedin"></i>
+            </span>
+          </div>
+          <div className="aboutsecondchild">
+            <img id="aboutsec" src={aboutsec2} alt="aboutsecond" />
+            <h1>Will Smith</h1>
+            <p>Product Designer</p>
+            <span>
+              <i class="bi bi-instagram"></i>
+            </span>
+            <span>
+              <i class="bi bi-whatsapp"></i>
+            </span>
+            <span>
+              <i class="bi bi-linkedin"></i>
+            </span>
+          </div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="aboutthird">
+          <div className="aboutthridchild">
+            <div className="aboutthirdimg">
+              <div className="aboutthirdimg1">
+                <i class="bi bi-truck"></i>
+              </div>
+            </div>{" "}
+            <br />
+            <h4>FREE AND FAST DELIVERY</h4>
+            <p>Free delivery for all orders over $140</p>
+          </div>
+          <div className="aboutthridchild">
+            <div className="aboutthirdimg">
+              <div className="aboutthirdimg1">
+                <i class="bi bi-headset"></i>
+              </div>
+            </div>{" "}
+            <br />
+            <h4>24/7 CUSTOMER SERVICE</h4>
+            <p>Friendly 24/7 customer support</p>
+          </div>
+          <div className="aboutthridchild">
+            <div className="aboutthirdimg">
+              <div className="aboutthirdimg1">
+                <i class="bi bi-shield-check"></i>
+              </div>
+            </div>{" "}
+            <br />
+            <h4>MONEY BACK GUARANTEE</h4>
+            <p> We reurn money within 30 days</p>
+          </div>
         </div>
       </div>
     </>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import "../CSS/Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer">
+      <div className="footer1">
+        <div className="footerchild">
+          <h1>Exclusive</h1> <br />
+          <h6>Subscribe</h6>
+          <p>Get 10% off your first order</p>
+          <input type="email" placeholder="Enter your Email" />
+        </div>
+        <div className="footerchild">
+          <h1>Support</h1> <br />
+          <p>
+            111 Bijoy sarani, Dhaka, <br /> DH 1515, Bangladesh
+          </p>
+          <p>exclusive@gmail.com</p>
+          <p>+88015-88888 9999</p>
+        </div>
+        <div className="footerchild">
+          <h1>Account</h1> <br />
+          <p>My Account</p>
+          <p>Login / Register </p>
+          <p>Cart</p>
+          <p> Wishlist</p>
+          <p>Shop</p>
+        </div>
+        <div className="footerchild">
+          <h1>Quick Link</h1> <br />
+          <p>Privacy Policy</p>
+          <p>Terms Of Use</p>
+          <p>FAQ</p>
+          <p>Contact</p>
+        </div>
+        <div className="footerchild">
+          <h1>Dowload App</h1> <br />
+          <p id="p-save">save s3 with App New User Only</p>
+        </div>
+      </div>{" "}
+      <div className="footer2">
+        <div className="footercopy">
+          <p>&copy;CopyRight Rimel 2022 All Right Reserved</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
